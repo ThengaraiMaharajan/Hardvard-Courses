d6d6f6bb0f572310a848d8ff3ba1a0b8585de799
DESKTOP-81HU2NM\TMS <tmaharajan1999@gmail.com>
2025-01-01 14:46:44 +0530

    code commit

---
+++ CS50's Introduction to Computer Science/week 6-Python/mario.py
@@ -0,0 +1,46 @@
+# iteration = int(input("Height: "))
+# for i in range(iteration):
+#     print("#")
+
+# while True:
+#     try:
+#         iteration = int(input("Height: "))
+#         if iteration > 0:
+#             break
+#     except ValueError:
+#         # print("Not an integer. Please try again.")
+#         pass
+
+# for i in range(iteration):
+#     print("#")
+
+# while True:
+#     try:
+#         iteration = int(input("width: "))
+#         if iteration > 0:
+#             break
+#     except ValueError:
+#         # print("Not an integer. Please try again.")
+#         pass
+
+# for i in range(iteration):
+#     print("#", end="")
+# print()
+
+
+# while True:
+#     try:
+#         iteration = int(input("width: "))
+#         if iteration > 0:
+#             break
+#     except ValueError:
+#         # print("Not an integer. Please try again.")
+#         pass
+
+# for i in range(3):
+#     for j in range(3):
+#         print("#", end="")
+#     print()
+
+# for i in range(3):
+#     print("#" * 3)
